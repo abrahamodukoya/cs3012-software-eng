e30d16e3a66d0cddc75980dd882094453711d6b1
abrahamodukoya <abrahamodukoya@yahoo.ie>
2018-09-20 13:41:01 +0100

    Can add a single edge to the graph

--- lca/graph.py
+++ lca/graph.py
@@ -2,3 +2,10 @@ class Graph:
 
     def __init__(self, V):
         self.adjList = [[] for i in range(V)]
+        self.V = V
+        self.E = 0
+
+    #adds a directed edge from u to v
+    def add_edge(self, u, v):
+        self.adjList[u].append(v)
+        self.E = self.E + 1
--- lca/test.py
+++ lca/test.py
@@ -1,11 +1,22 @@
 import unittest
 from graph import Graph
 
-class LCATest (unittest.TestCase):
+class LCATest(unittest.TestCase):
+
+    def setUp(self):
+        self.graphSize = 10
+        self.aGraph = Graph(self.graphSize)
 
     def test_create_graph(self):
-        aGraph = Graph(5)
-        self.assertEqual(len(aGraph.adjList), 5)
+        self.assertEqual(len(self.aGraph.adjList), self.graphSize)
+
+    def test_add_edge(self):
+        self.aGraph.add_edge(0, 1)
+        self.assertEqual(self.aGraph.E, 1)
+        self.assertEqual(self.aGraph.adjList[0], [1])
+
+    def test_add_multiple_edges(self):
+        pass
 
 
 if __name__ == '__main__':
