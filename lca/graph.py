18419a1992877a89a437bc1d07f3db9e58c58cf0
abrahamodukoya <abrahamodukoya@yahoo.ie>
2018-09-20 13:23:08 +0100

    Added Graph class

---
+++ lca/graph.py
@@ -0,0 +1,4 @@
+class Graph:
+
+    def __init__(self, V):
+        self.adjList = [[] for i in range(V)]
--- lca/test.py
+++ lca/test.py
@@ -1,7 +1,12 @@
 import unittest
+from graph import Graph
 
 class LCATest (unittest.TestCase):
 
+    def test_create_graph(self):
+        aGraph = Graph(5)
+        self.assertEqual(len(aGraph.adjList), 5)
+
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
