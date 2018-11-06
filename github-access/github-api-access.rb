a8c9fdaa9ad81ab054bfcfe0be86e3ed68891a95
abrahamodukoya <abrahamodukoya@yahoo.ie>
2018-11-06 14:10:48 +0000

    can make requests to the api for the logged in user

--- github-access/github-api-access.rb
+++ github-access/github-api-access.rb
@@ -1,15 +1,22 @@
 require 'uri'
 require 'json'
 require 'net/http'
+require "io/console"
 
 puts "Enter your GitHub username:"
 user = gets
 user.chomp!
-github_api_uri = URI::HTTPS.build(host: "api.github.com", path: "/users/" + user)
-json_result = Net::HTTP.get(github_api_uri)
-result = JSON.parse(json_result)
-if result.has_key?("message")
-  puts result["message"]
-else
-  puts result["name"]
+puts "Enter your GitHub password:"
+password = STDIN.noecho(&:gets).chomp
+github_api_uri = URI.parse("https://api.github.com/user")
+request = Net::HTTP::Get.new(github_api_uri)
+request.basic_auth(user, password)
+req_options = {
+  use_ssl: github_api_uri.scheme == "https",
+}
+response = Net::HTTP.start(github_api_uri.hostname, github_api_uri.port, req_options) do |http|
+  http.request(request)
 end
+json_result = response.body
+result = JSON.parse(json_result)
+puts json_result
