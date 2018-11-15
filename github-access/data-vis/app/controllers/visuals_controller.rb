98aa6c2aee9771b9509a430ab532b8924779aeb0
abrahamodukoya <abrahamodukoya@yahoo.ie>
2018-11-15 15:52:54 +0000

    users can now be saved in the db

--- github-access/data-vis/app/controllers/visuals_controller.rb
+++ github-access/data-vis/app/controllers/visuals_controller.rb
@@ -4,18 +4,19 @@ class VisualsController < ApplicationController
 def create
   #TODO: make a call to Github API here to see if the entered repo
   #exists; if it does save it, else display error
-  #FIX: entering users into db
   username = params[:repo]["username"]
   password = params[:repo]["password"]
   repo = params[:repo]["name"]
   if user_has_repo? username, password, repo
     @repo = Repo.new(repos_params)
     @repo.save
-    # contributors = get_contributors username, password, repo
-    # contributors.each do |user|
-    #   @user = User.new(user["username"])
-    #   @user.save
-    # end
+    contributors = get_contributors username, password, repo
+    puts contributors
+     contributors.each do |user|
+       @user = User.new
+       @user.username = user["login"]
+       @user.save
+     end
     redirect_to action: "index"
   else
     render 'new'
@@ -24,7 +25,7 @@ end
 
 def index
   @repos = Repo.all
-  #@users = User.all
+  @users = User.all
 end
 
 def show
@@ -37,5 +38,7 @@ private
   def repos_params
     params.require(:repo).permit(:name)
   end
+
+  private
 
 end
